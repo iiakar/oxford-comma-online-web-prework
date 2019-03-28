4220d381ac77d170123798b31ef076232d0a6487
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-28 13:34:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
 def oxford_comma(array)
+  return array.first if array.size == 1
 
-["fiddleheads","okra","kohlrabi"]
+if array.size == 2
+    array.insert(1, "and").join(" ")
 
-"fiddleheads, okra, and kohlrabi"
-
-
-end
+  elsif array.size == 3
+    array.insert("," "and ")
+    array.join(", ")
+  elsif array.size > 3 
+    return array.first 
+  end 
+end 
