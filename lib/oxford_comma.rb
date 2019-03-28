7b3ef8b9b3f310186180cbd8a2ed062f49b0130f
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-28 14:52:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array) 
   oxford_comma= Array.new
-  oxford_comma return array if Array= Array 
+  oxford_comma return Array if Array= Array 
   
   elsif 
   
+  
+  
+  it "adds 'and' between elements when given a 2-element array" do
+   
     
