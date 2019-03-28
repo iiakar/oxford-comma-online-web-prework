db4e1d622d185978769b3aa910ebc1eff44822ba
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-28 14:54:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   oxford_comma return Array if Array= Array 
   
   elsif 
-  
+  Array.size=2 include 'and' b
   
   it "adds 'and' between elements when given a 2-element array" do
    
