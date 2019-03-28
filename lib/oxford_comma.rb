412682bc40d535ee9b307af52d0b52cfb7233516
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-28 15:03:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,9 @@ def oxford_comma(array)
 elsif
 array.size=3.include "," starfruit."and"
 elsif
+Array.size>3 re
+
+
 
 it "correctly formats arrays of lengths greater than three" do
     expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
