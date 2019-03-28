671449b15c13de79cb3e1a5bb3891274a0e83dda
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-28 13:40:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   return array.first if array.size == 1
 
 if array.size == 2
-    array.insert("and")
+    array.insert("and").join(" ")
 
   elsif array.size == 3
     array.insert("," "and ")
